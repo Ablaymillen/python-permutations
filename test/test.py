24d5ffc21e423d9e905acf6192c103a9d656ac7a
Ablaymillen <80356093+Ablaymillen@users.noreply.github.com>
2021-09-22 23:03:41 +0600

    Update and rename test.txt to test.py

---
+++ test/test.py
@@ -0,0 +1,5 @@
+from src.permutation import permutation 
+#returns permutation shuffle with passed item
+#--------------- returns all permutations with the passed list items -----------------#
+for item in permutation(list):
+    print(item)
