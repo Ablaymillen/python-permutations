6f569363fd34d2e5159a0aa5ff731498e499b016
Ablaymillen <80356093+Ablaymillen@users.noreply.github.com>
2021-09-22 23:13:36 +0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-from srs import perm as p
+from src import perm as p
 #returns permutation shuffle with passed item
 #--------------- returns all permutations with the passed list items -----------------#
 list = ['a', 'b', 'c']
